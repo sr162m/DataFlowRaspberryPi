0f91e33c07db928b123af246375d51600ffe864c
sr162m <47257851+sr162m@users.noreply.github.com>
2019-08-01 09:38:58 -0500

    Update python_example
    - Added random functionality to enable data changes

--- python_example.py
+++ python_example.py
@@ -1,17 +1,17 @@
 import requests
 import time
+import random
 
 dataFlowObject={}
 
 url = 'YOUR ENDPOINT HERE'
 
 while True:
-	dataFlowObject["deviceID"] = "909456"
-	dataFlowObject["name"] = "Raspberry_Pi"
-	dataFlowObject["tempF"] = "98.6"
-	dataFlowObject["humidity"] = "56"
-	dataFlowObject["windspeed"] = "55"
-	requests.post(url, json=dataFlowObject)
-	time.sleep(10)
-
+    dataFlowObject["deviceID"] = "909456"
+    dataFlowObject["name"] = "Raspberry_Pi"
+    dataFlowObject["tempF"] = str(random.randint(55,120))
+    dataFlowObject["humidity"] = str(random.randint(30,100))
+    dataFlowObject["windspeed"] = str(random.randint(0,200))
+    requests.post(url, verify=False, json=dataFlowObject)
+    #time.sleep(10)
 
